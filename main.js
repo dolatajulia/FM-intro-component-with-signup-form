38abb7ed39ce8a7d2ad00f4b6e314c9d2d0e9227
jdolata <juliadolata007@gmail.com>
2022-09-28 16:57:44 +0200

    improve js validation

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const email = document.getElementById('email');
 const name = document.getElementById('name');
 const lastName = document.getElementById('last-name');
 const password = document.getElementById('password');
+const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
 form.addEventListener('submit', (e) => {
 
@@ -17,6 +18,13 @@ form.addEventListener('submit', (e) => {
         document.getElementById('btn').style.backgroundColor = 'var(--accent-col-green)';
     }
 
+    if(name.value != '' && name.value != null){
+        document.getElementById('error-icon-name').style.display = 'none';
+        document.getElementById('error-message-name').style.display = 'none';
+        document.getElementById('name').style.border = 'solid 1px var(--fc-light)';
+        document.getElementById('name').style.color = 'var(--fc-dark)';
+    }
+
     if(lastName.value == ''|| lastName.value == null){
         isThereAnError = true;
         document.getElementById('error-icon-last-name').style.display = 'block';
@@ -26,13 +34,27 @@ form.addEventListener('submit', (e) => {
         document.getElementById('btn').style.backgroundColor = 'var(--accent-col-green)';
     }
 
-    if(email.value == ''|| email.value == null){
+    if(lastName.value != '' && lastName.value != null){
+        document.getElementById('error-icon-last-name').style.display = 'none';
+        document.getElementById('error-message-last-name').style.display = 'none';
+        document.getElementById('last-name').style.border = 'solid 1px var(--fc-light)';
+        document.getElementById('last-name').style.color = 'var(--fc-dark)';
+    }
+
+    if(email.value == ''|| email.value == null || !email.value.match(emailPattern)){
         isThereAnError = true;
         document.getElementById('error-icon-email').style.display = 'block';
         document.getElementById('error-message-email').style.display = 'block';
         document.getElementById('email').style.border = 'solid 1px var(--bg-col)';
         document.getElementById('email').style.color = 'var(--bg-col)';
         document.getElementById('btn').style.backgroundColor = 'var(--accent-col-green)';
+    }
+
+    if(email.value != '' && email.value != null && email.value.match(emailPattern)){
+        document.getElementById('error-icon-email').style.display = 'none';
+        document.getElementById('error-message-email').style.display = 'none';
+        document.getElementById('email').style.border = 'solid 1px var(--fc-light)';
+        document.getElementById('email').style.color = 'var(--fc-dark)';
     }
 
     if(password.value == ''|| password.value == null){
@@ -42,6 +64,13 @@ form.addEventListener('submit', (e) => {
         document.getElementById('password').style.border = 'solid 1px var(--bg-col)';
         document.getElementById('password').style.color = 'var(--bg-col)';
         document.getElementById('btn').style.backgroundColor = 'var(--accent-col-green)';
+    }
+
+    if(password.value != '' && password.value != null){
+        document.getElementById('error-icon-password').style.display = 'none';
+        document.getElementById('error-message-password').style.display = 'none';
+        document.getElementById('password').style.border = 'solid 1px var(--fc-light)';
+        document.getElementById('password').style.color = 'var(--fc-dark)';
     }
 
     if (isThereAnError){
